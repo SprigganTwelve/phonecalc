7bfc53f76a0904810aa13629a54dcfca248649b8
Paterne <angepaterne.dali@gmail.com>
2024-04-30 21:09:15 +0200

    reduct the code of creating button widget

--- android/phone_calc_android/lib/widgets/buttons.dart
+++ android/phone_calc_android/lib/widgets/buttons.dart
@@ -5,21 +5,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class CreateButton extends StatelessWidget {
-  const CreateButton({super.key});
+  final String text;
+  CreateButton({required this.text});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
-
-class ToLeft extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return buttonRightBox();
-  }
-
-  Widget _createButton(String text) {
     return Padding(
       padding: const EdgeInsets.all(7),
       child: TextButton(
@@ -36,6 +26,14 @@ class ToLeft extends StatelessWidget {
             style: const TextStyle(color: Colors.white, fontSize: 30)),
       ),
     );
+    ;
+  }
+}
+
+class ToLeft extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return buttonRightBox();
   }
 
   Widget buttonRightBox() {
@@ -43,43 +41,43 @@ class ToLeft extends StatelessWidget {
       children: [
         Row(
           children: [
-            _createButton("e"),
-            _createButton("µ"),
-            _createButton("sin"),
+            CreateButton(text: "e"),
+            CreateButton(text: "µ"),
+            CreateButton(text: "sin"),
           ],
         ),
         Row(
           children: [
-            _createButton("Ac"),
-            _createButton("⌫"),
-            _createButton("/"),
+            CreateButton(text: "Ac"),
+            CreateButton(text: "⌫"),
+            CreateButton(text: "/"),
           ],
         ),
         Row(
           children: [
-            _createButton("7"),
-            _createButton("8"),
-            _createButton("9"),
+            CreateButton(text: "7"),
+            CreateButton(text: "8"),
+            CreateButton(text: "9"),
           ],
         ),
         Row(
           children: [
-            _createButton("4"),
-            _createButton("5"),
-            _createButton("6"),
+            CreateButton(text: "4"),
+            CreateButton(text: "5"),
+            CreateButton(text: "6"),
           ],
         ),
         Row(
           children: [
-            _createButton("1"),
-            _createButton("2"),
-            _createButton("3"),
+            CreateButton(text: "1"),
+            CreateButton(text: "2"),
+            CreateButton(text: "3"),
           ],
         ),
         Row(
           children: [
-            SizedBox(width: 205, child: _createButton("0")),
-            SizedBox(width: 100, child: _createButton(".")),
+            SizedBox(width: 205, child: CreateButton(text: "0")),
+            SizedBox(width: 100, child: CreateButton(text: ".")),
           ],
         ),
       ],
@@ -93,32 +91,13 @@ class ToRight extends StatelessWidget {
     return buttonLeftBox();
   }
 
-  Widget _createButton(String text) {
-    return Padding(
-      padding: const EdgeInsets.all(7),
-      child: TextButton(
-        onPressed: () => {},
-        style: ButtonStyle(
-          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-          ),
-          fixedSize: MaterialStateProperty.all(const Size(90, 90)),
-          alignment: Alignment.center,
-          backgroundColor: MaterialStateProperty.all(const Color(0xFFBAD7EC)),
-        ),
-        child: Text(text,
-            style: const TextStyle(color: Colors.white, fontSize: 30)),
-      ),
-    );
-  }
-
   Widget buttonLeftBox() {
     return Column(
       children: [
-        _createButton("deg"),
-        _createButton("*"),
-        SizedBox(height: 150, child: _createButton("+")),
-        Expanded(flex: 1, child: _createButton("=")),
+        CreateButton(text: "deg"),
+        CreateButton(text: "*"),
+        SizedBox(height: 150, child: CreateButton(text: "+")),
+        Expanded(flex: 1, child: CreateButton(text: "=")),
       ],
     );
   }
